85586ba3c4ab1ed9ad8bb2656477698528cd1872
YuzuMz <patipolz.15@gmail.com>
2026-05-28 01:16:17 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+function calculate() {
+   
+    const num1 = parseFloat(document.getElementById('num1').value);
+    const num2 = parseFloat(document.getElementById('num2').value);
+    const operator = document.getElementById('operator').value;
+    const resultElement = document.getElementById('result');
+
+    let result = 0;
+
+    
+    switch (operator) {
+        case 'add':
+            result = num1 + num2;
+            break;
+        case 'subtract':
+            result = num1 - num2;
+            break;
+        case 'multiply':
+            result = num1 * num2;
+            break;
+        case 'divide':
+            result = num1 / num2;
+            break;
+       
+    }
+ 
+    resultElement.innerText = result;
+}
+
